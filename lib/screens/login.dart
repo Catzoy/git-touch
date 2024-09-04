6edc9aa3d979d9aaa1b8f85ef23b84ae73bcdbfc
Dmytro Demchenko <demchenko20021@gmail.com>
2024-09-04 18:51:14 +0300

    Separate login auth from login screen

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -26,163 +26,172 @@ part 'login_gt_popup.dart';
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final auth = Provider.of<AuthModel>(context);
-    final theme = Provider.of<ThemeModel>(context);
-
     return SingleScaffold(
       title: Text(AppLocalizations.of(context)!.selectAccount),
-      body: auth.loading
-          ? const Center(child: Loading())
-          : Column(
-              children: [
-                AntList(
-                  children: [
-                    for (final (index, _) in auth.accounts.indexed)
-                      LoginAccountTile(
-                        index: index,
-                      ),
-                    LoginAddAccountTile(
-                      text: AppLocalizations.of(context)!.githubAccount,
-                      brand: Ionicons.logo_github,
-                      onTap: () async {
-                        theme.showActions(context, [
-                          ActionItem(
-                            text: 'via OAuth',
-                            onTap: (_) {
-                              auth.redirectToGithubOauth();
-                            },
-                          ),
-                          ActionItem(
-                            text: 'via OAuth (Public repos only)',
-                            onTap: (_) {
-                              auth.redirectToGithubOauth(true);
-                            },
-                          ),
-                          ActionItem(
-                            text: 'via Personal token',
-                            onTap: (_) async {
-                              final token = await requestGithubToken(
-                                context: context,
-                              );
-                              if (token != null) {
-                                try {
-                                  await auth.loginWithToken(token);
-                                } catch (err) {
-                                  context.showError(err);
-                                }
-                              }
-                            },
-                          ),
-                        ]);
-                      },
-                    ),
-                    LoginAddAccountTile(
-                      text: AppLocalizations.of(context)!.gitlabAccount,
-                      brand: Ionicons.git_branch_outline,
-                      onTap: () async {
-                        final result = await requestGitlabAuth(
-                          context: context,
-                        );
-                        if (result != null) {
-                          try {
-                            await auth.loginToGitlab(
-                              result.domain,
-                              result.token,
-                            );
-                          } catch (err) {
-                            context.showError(err);
-                          }
-                        }
-                      },
-                    ),
-                    LoginAddAccountTile(
-                      text: AppLocalizations.of(context)!.bitbucketAccount,
-                      brand: Ionicons.logo_bitbucket,
-                      onTap: () async {
-                        final result = await requestBitbucketAuth(
-                          context: context,
-                        );
-                        if (result != null) {
-                          try {
-                            await auth.loginToBb(
-                              result.domain,
-                              result.username,
-                              result.password,
-                            );
-                          } catch (err) {
-                            context.showError(err);
-                          }
-                        }
-                      },
-                    ),
-                    LoginAddAccountTile(
-                      text: AppLocalizations.of(context)!.giteaAccount,
-                      brand: Ionicons.git_branch_outline, // TODO: brand icon
-                      onTap: () async {
-                        final result = await requestGiteaAuth(
-                          context: context,
-                        );
-                        if (result != null) {
-                          try {
-                            await auth.loginToGitea(
-                              result.domain,
-                              result.token,
-                            );
-                          } catch (err) {
-                            context.showError(err);
-                          }
-                        }
-                      },
-                    ),
-                    LoginAddAccountTile(
-                      text: '${AppLocalizations.of(context)!.giteeAccount}(码云)',
-                      brand: Ionicons.git_branch_outline, // TODO: brand icon
-                      onTap: () async {
-                        final token = await requestGiteeToken(
-                          context: context,
-                        );
-                        if (token != null) {
-                          try {
-                            await auth.loginToGitee(token);
-                          } catch (err) {
-                            context.showError(err);
-                          }
-                        }
-                      },
-                    ),
-                    LoginAddAccountTile(
-                      text: 'Gogs Account',
-                      brand: Ionicons.git_branch_outline, // TODO: brand icon
-                      onTap: () async {
-                        final result = await requestGogsToken(
-                          context: context,
-                        );
-                        if (result != null) {
-                          try {
-                            await auth.loginToGogs(
-                              result.domain,
-                              result.token,
-                            );
-                          } catch (err) {
-                            context.showError(err);
-                          }
-                        }
-                      },
-                    ),
-                  ],
-                ),
-                Container(
-                  padding: CommonStyle.padding,
-                  child: Text(
-                    'Swipe to left to remove account',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: AntTheme.of(context).colorTextSecondary,
-                    ),
+      body: const _LoginAuth(),
+    );
+  }
+}
+
+class _LoginAuth extends StatelessWidget {
+  const _LoginAuth({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final auth = Provider.of<AuthModel>(context);
+    if (auth.loading) {
+      return const Center(child: Loading());
+    }
+
+    return Column(
+      children: [
+        AntList(
+          children: [
+            for (final (index, _) in auth.accounts.indexed)
+              LoginAccountTile(
+                index: index,
+              ),
+            LoginAddAccountTile(
+              text: AppLocalizations.of(context)!.githubAccount,
+              brand: Ionicons.logo_github,
+              onTap: () async {
+                context.read<ThemeModel>().showActions(context, [
+                  ActionItem(
+                    text: 'via OAuth',
+                    onTap: (_) {
+                      auth.redirectToGithubOauth();
+                    },
                   ),
-                )
-              ],
+                  ActionItem(
+                    text: 'via OAuth (Public repos only)',
+                    onTap: (_) {
+                      auth.redirectToGithubOauth(true);
+                    },
+                  ),
+                  ActionItem(
+                    text: 'via Personal token',
+                    onTap: (_) async {
+                      final token = await requestGithubToken(
+                        context: context,
+                      );
+                      if (token != null) {
+                        try {
+                          await auth.loginWithToken(token);
+                        } catch (err) {
+                          context.showError(err);
+                        }
+                      }
+                    },
+                  ),
+                ]);
+              },
             ),
+            LoginAddAccountTile(
+              text: AppLocalizations.of(context)!.gitlabAccount,
+              brand: Ionicons.git_branch_outline,
+              onTap: () async {
+                final result = await requestGitlabAuth(
+                  context: context,
+                );
+                if (result != null) {
+                  try {
+                    await auth.loginToGitlab(
+                      result.domain,
+                      result.token,
+                    );
+                  } catch (err) {
+                    context.showError(err);
+                  }
+                }
+              },
+            ),
+            LoginAddAccountTile(
+              text: AppLocalizations.of(context)!.bitbucketAccount,
+              brand: Ionicons.logo_bitbucket,
+              onTap: () async {
+                final result = await requestBitbucketAuth(
+                  context: context,
+                );
+                if (result != null) {
+                  try {
+                    await auth.loginToBb(
+                      result.domain,
+                      result.username,
+                      result.password,
+                    );
+                  } catch (err) {
+                    context.showError(err);
+                  }
+                }
+              },
+            ),
+            LoginAddAccountTile(
+              text: AppLocalizations.of(context)!.giteaAccount,
+              brand: Ionicons.git_branch_outline, // TODO: brand icon
+              onTap: () async {
+                final result = await requestGiteaAuth(
+                  context: context,
+                );
+                if (result != null) {
+                  try {
+                    await auth.loginToGitea(
+                      result.domain,
+                      result.token,
+                    );
+                  } catch (err) {
+                    context.showError(err);
+                  }
+                }
+              },
+            ),
+            LoginAddAccountTile(
+              text: '${AppLocalizations.of(context)!.giteeAccount}(码云)',
+              brand: Ionicons.git_branch_outline, // TODO: brand icon
+              onTap: () async {
+                final token = await requestGiteeToken(
+                  context: context,
+                );
+                if (token != null) {
+                  try {
+                    await auth.loginToGitee(token);
+                  } catch (err) {
+                    context.showError(err);
+                  }
+                }
+              },
+            ),
+            LoginAddAccountTile(
+              text: 'Gogs Account',
+              brand: Ionicons.git_branch_outline, // TODO: brand icon
+              onTap: () async {
+                final result = await requestGogsToken(
+                  context: context,
+                );
+                if (result != null) {
+                  try {
+                    await auth.loginToGogs(
+                      result.domain,
+                      result.token,
+                    );
+                  } catch (err) {
+                    context.showError(err);
+                  }
+                }
+              },
+            ),
+          ],
+        ),
+        Container(
+          padding: CommonStyle.padding,
+          child: Text(
+            'Swipe to left to remove account',
+            style: TextStyle(
+              fontSize: 16,
+              color: AntTheme.of(context).colorTextSecondary,
+            ),
+          ),
+        )
+      ],
     );
   }
 }
